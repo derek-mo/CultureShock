e1992e267f46fad1c4fbbad06eae28aaec58e729
Derek Mo <derek.mo7203@gmail.com>
2023-01-28 12:42:36 -0500

    ruh

--- CultureShock.py
+++ CultureShock.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from bs4 import BeautifulSoup
 
 with open("index.html") as fp:
@@ -6,7 +5,5 @@ with open("index.html") as fp:
 
 #soup = BeautifulSoup
  
-=======
 from selenium import webdriver
 PATH = "C:\Program Files (x86)\chromedriver.exe"
->>>>>>> 6387fca826c498e9b25ec93d2bca1d6cc2604b8e
