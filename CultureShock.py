909dac1313e4a99193636e011cc66a45aa45879e
Alan Wang <alanwang611@gmail.com>
2023-01-28 17:37:03 -0500

    change country list

--- CultureShock.py
+++ CultureShock.py
@@ -24,9 +24,10 @@ driver.maximize_window()
 
 driver.get("https://www.worldcountriesforkids.com/" + countryName + "/")
 fact = driver.find_element(By.CLASS_NAME,"brief-history-left").text
-#print(fact)
 fact_list = []
 temp = ""
+
+#accesses the fact file
 for x in range(0,len(fact)):
     if fact[x] == '\n':
         fact_list.append(temp)
@@ -34,7 +35,37 @@ for x in range(0,len(fact)):
     else:
         temp = temp + fact[x]
 fact_sheet = []
+
+#removes the whitespace to the list
 for y in fact_list:
     fact_sheet.append([y.lstrip()])
-print(fact_sheet)
+
+#access food element
+food = driver.page_source
+temp = ""
+for i in range (0, len(food) - 4):
+    if (food[i] == 'C') & (food[i+1] == 'u') & (food[i+2] == 'i') & (food[i+3] == 's') & (food[i + 4] == 'i'):
+        for x in range (-500,700):
+            temp = temp + food[i + x]
+        break
+
+#creating a list to store the data in: link, cuisine, description, and link description
+food_list = []
+
+#index of the link start
+link_index_start = temp.find("img src=")
+link_index_end = temp.find(".png")
+food_list.append(temp[link_index_start + 9:link_index_end + 4:1])
+print(food_list)
+
+#index of the description start
+food_description_start = temp.find("<p>")
+food_description_end = temp[food_description_start + 10: len(temp): 1].find("<p>")
+#print(temp[food_description_start:food_description_end:1])
+print(food_description_start)
+print(food_description_end)
+print(temp[food_description_start: food_description_end:1])
+#print(temp[food_description_start: food_description_end:1])
+#print(temp)
+#quit the driver
 driver.quit()
